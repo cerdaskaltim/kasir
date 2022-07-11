a15e707334b0759e5d593e1fd28b3a2ebcc72cf0
cerdaskaltim <108891227+cerdaskaltim@users.noreply.github.com>
2022-07-11 10:11:14 +0800

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/kategori', 'App\Http\Controllers\CategoryController')->except([
+  'create', 'show'
+]);
+
+
+

bf3bc3106985c5ce10914d17e91c38be5535dc1e
cerdaskaltim <108891227+cerdaskaltim@users.noreply.github.com>
2022-07-11 10:07:09 +0800

    Add files via upload

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::orderBy('created_at', 'DESC')->paginate(10);
+        return view('categories.index', compact('categories'));
+    }
+}
+
+
+
+
